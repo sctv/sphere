8d9d61c1c46f2d376c8f5cdc83417ae65aee95cd
Travis <sab.gaby@gmail.com>
2018-02-06 13:35:15 +0000

    Travis: #485

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,1 +1,43 @@
-$(document).ready(function(){$(".owl-carousel").owlCarousel({center:!0,items:4,loop:!0,margin:0,nav:!1,dots:!1,autoplay:!0,responsive:{0:{items:1},700:{items:2},1000:{items:3},1400:{items:4}}});var e=$(".owl-carousel");e.owlCarousel(),$(".customNextBtn").click(function(){e.trigger("next.owl.carousel")}),$(".customPrevBtn").click(function(){e.trigger("prev.owl.carousel")}),$(".beginningBtn").click(function(){e.trigger("to.owl.carousel",[0])})}),$(".hero .image").dimmer("show");
+$(document).ready(function() {
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items: 4,
+    loop:true,
+    margin:0,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        700:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        },
+        1400:{
+            items:4,
+        }
+    }
+});
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+    // Go to the next item
+    $('.customNextBtn').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.customPrevBtn').click(function() {
+        owl.trigger('prev.owl.carousel');
+    })
+
+    $('.beginningBtn').click(function() {
+        owl.trigger('to.owl.carousel', [0]);
+    })
+
+});
+
+$('.hero .image').dimmer('show');
--- assets/js/githubComments.js
+++ assets/js/githubComments.js
@@ -1,1 +1,80 @@
-function ParseLinkHeader(e){var a=e.split(","),s={};for(var t in a){var n=a[t],i={};i.name=n.match(/rel="([^"]*)/)[1],i.url=n.match(/<([^>]*)/)[1],i.page=n.match(/page=(\d+).*$/)[1],s[i.name]=i}return s}function ShowComments(e,a,s){var t="https://api.github.com/repos/"+e+"/issues/"+a+"/comments?page="+s;$.ajax(t,{headers:{Accept:"application/vnd.github.v3.html+json"},dataType:"json",success:function(t,n,i){if(1==s){var o="https://github.com/"+e+"/issues/"+a+"#new_comment_field";$("#gh-comments-list").append("<a class='noelink' href='"+o+"' target='_blank'><h3 class='ui grey header'>Comments from GitHub<div class='ui divider xo marginless'></div><div class='sub header'><i class='far fa-comment'></i>&nbsp;Add a Comment to GitHub Issue</div></h3></a> ")}($.each(t,function(e,a){var s=new Date(a.created_at),t="<div class='ui small comments'>";t+="<div class='comment'>",t+="<a class='avatar'>",t+="<img src='"+a.user.avatar_url+"'>",t+="</a>",t+="<div class='content'>",t+="<a class='author noelink noul' href='"+a.user.html_url+"'>"+a.user.login+"</a>",t+="<div class='metadata'>",t+="<div class='date'>"+s.toUTCString()+"</div>",t+="</div>",t+="<div class='text'>",t+=a.body_html,t+="</div>",t+="</div>",t+="</div>",$("#gh-comments-list").append(t)}),i.getResponseHeader("Link"))&&("next"in ParseLinkHeader(i.getResponseHeader("Link"))&&ShowComments(e,a,s+1))},error:function(){$("#gh-comments-list").append("Comments are not open for this post yet.")}})}function DoGithubComments(e,a){$(document).ready(function(){ShowComments(e,a,1)})}
+// Original code taken with permission from : https://github.com/dwilliamson/donw.io/blob/master/public/js/github-comments.js
+
+// use of ajax vs getJSON for headers use to get markdown (body vs body_html)
+
+function ParseLinkHeader(lnk)
+{
+    var entries = lnk.split(",");
+    var links = { };
+    for (var i in entries)
+    {
+        var entry = entries[i];
+        var link = { };
+        link.name = entry.match(/rel="([^"]*)/)[1];
+        link.url = entry.match(/<([^>]*)/)[1];
+        link.page = entry.match(/page=(\d+).*$/)[1];
+        links[link.name] = link;
+    }
+    return links;
+}
+
+function DoGithubComments(repo_name, comment_id)
+{
+    $(document).ready(function ()
+    {
+        ShowComments(repo_name, comment_id, 1);
+    });
+}
+
+
+function ShowComments(repo_name, comment_id, page_id)
+{
+    var api_comments_url = "https://api.github.com/repos/" + repo_name + "/issues/" + comment_id + "/comments" + "?page=" + page_id;
+
+    $.ajax(api_comments_url, {
+        headers: {Accept: "application/vnd.github.v3.html+json"},
+        dataType: "json",
+        success: function(comments, textStatus, jqXHR) {
+
+            // Add post button to first page
+            if (page_id == 1)
+            {
+                var url = "https://github.com/" + repo_name + "/issues/" + comment_id + "#new_comment_field";
+                $("#gh-comments-list").append("<a class='noelink' href='" + url + "' target='_blank'><h3 class='ui grey header'>Comments from GitHub<div class='ui divider xo marginless'></div><div class='sub header'><i class='far fa-comment'></i>&nbsp;Add a Comment to GitHub Issue</div></h3></a> ");
+            }
+
+            // Individual comments
+            $.each(comments, function(i, comment) {
+
+                var date = new Date(comment.created_at);
+
+                var t = "<div class='ui small comments'>";
+                t += "<div class='comment'>";
+                t += "<a class='avatar'>";
+                t += "<img src='" + comment.user.avatar_url + "'>";
+                t += "</a>";
+                t += "<div class='content'>";
+                t += "<a class='author noelink noul' href='" + comment.user.html_url + "'>" + comment.user.login + "</a>";
+                t += "<div class='metadata'>";
+                t += "<div class='date'>" + date.toUTCString() + "</div>";
+                t += "</div>"; //close metadata
+                t += "<div class='text'>";
+                t += comment.body_html;
+                t += "</div>"; //close text
+                t += "</div>"; //close content
+                t += "</div>"; //close comment
+                $("#gh-comments-list").append(t);
+            });
+
+            // Call recursively if there are more pages to display
+            var linksResponse = jqXHR.getResponseHeader("Link");
+            if (linksResponse) {
+                var links = ParseLinkHeader(jqXHR.getResponseHeader("Link"));
+                if ("next" in links)
+                {
+                    ShowComments(repo_name, comment_id, page_id+1);
+                }
+            }
+        }
+    });
+}
